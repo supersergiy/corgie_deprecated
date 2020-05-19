dd968615988ca2d7073dbca240d59e67e1a7940e
Sergiy Popovych <sergiy.popovich@gmail.com>
2020-05-18 17:01:36 -0700

    more requirements for docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="corgie",
-    version="0.0.2",
+    version="0.0.7",
     author="Sergiy Popovych",
     author_email="sergiy.popovich@gmail.com",
     description="Connectomics Registration General Inference Engine",
