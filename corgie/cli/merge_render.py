0647f9fa698da8dd125c6ce3257f7793c3fb94f2
Thomas Macrina <tmacrina@princeton.edu>
2021-02-01 23:47:03 +0000

    Adjust merge-render to allow more parameters from spec

--- corgie/cli/merge_render.py
+++ corgie/cli/merge_render.py
@@ -5,131 +5,124 @@ from corgie.log import logger as corgie_logger
 from corgie.layers import get_layer_types, DEFAULT_LAYER_TYPE, \
                              str_to_layer_type
 from corgie.boundingcube import get_bcube_from_coords
+from corgie.stack import Stack
 
 from corgie.argparsers import LAYER_HELP_STR, \
-        create_layer_from_spec, corgie_optgroup, corgie_option, \
-        create_stack_from_spec
+        create_layer_from_dict, corgie_optgroup, corgie_option
 import json
 
 class MergeRenderJob(scheduling.Job):
     def __init__(self, 
-                 src_stack, 
-                 dst_stack, 
+                 src_layers, 
+                 src_specs,
+                 dst_layer, 
                  mip,
                  pad,
                  bcube,
-                 z_list,
                  chunk_xy):
         """Render multiple images to the same destination image
         
         Args:
-            src_stack (Stack)
-            dst_stack (Stack)
+            src_layers ({'img':{Layers},
+            src_masks ([Layer])
+            dst_layer (Stack)
             mip (int)
             pad (int)
             bcube (BoundingCube)
             z_list ([int]): : ranked by layer priority (first image overwrites later images)
+            mask_ids ([int]) 
         """
 
-        self.src_stack = src_stack
-        self.dst_stack = dst_stack
+        self.src_layers = src_layers
+        self.src_specs = src_specs
+        self.dst_layer = dst_layer
         self.mip = mip
         self.pad = pad
         self.bcube = bcube
         self.chunk_xy = chunk_xy
-        self.z_list = z_list
-
         super().__init__()
 
     def task_generator(self):
-        chunks = self.dst_stack.get_layers()[0].break_bcube_into_chunks(
+        chunks = self.dst_layer.break_bcube_into_chunks(
                 bcube=self.bcube,
                 chunk_xy=self.chunk_xy,
                 chunk_z=1,
                 mip=self.mip)
 
-        tasks = [MergeRenderTask(src_stack=self.src_stack,
-                          dst_stack=self.dst_stack,
+        tasks = [MergeRenderTask(
+                          src_layers=self.src_layers,
+                          src_specs=self.src_specs,
+                          dst_layer=self.dst_layer,
                           mip=self.mip,
                           pad=self.pad,
-                          bcube=input_chunk,
-                          z_list=self.z_list) for input_chunk in chunks]
-        corgie_logger.info(f"Yielding render tasks for bcube: {self.bcube}, MIP: {self.mip}")
+                          bcube=input_chunk) for input_chunk in chunks]
+        corgie_logger.info(
+            f"Yielding render tasks for bcube: {self.bcube}, MIP: {self.mip}")
 
         yield tasks
 
 
 class MergeRenderTask(scheduling.Task):
     def __init__(self, 
-                 src_stack, 
-                 dst_stack, 
+                 src_layers,
+                 src_specs, 
+                 dst_layer, 
                  mip,
                  pad,
-                 bcube,
-                 z_list):
+                 bcube):
+
         """Render multiple images to the same destination image
         
         Args:
-            src_stack (Stack)
-            dst_stack (Stack)
+            src_layers ({Layer})
+            src_specs (json): list of dicts with img, field, mask, z, & mask_id per island
+                ranked by layer priority (first image overwrites later images)
+            dst_layer (Stack)
             mip (int)
             pad (int)
             bcube (BoundingCube)
-            z_list ([int]): : ranked by layer priority (first image overwrites later images)
         """
         super().__init__(self)
-        self.src_stack = src_stack
-        self.dst_stack = dst_stack
+        self.src_layers = src_layers
+        self.src_specs = src_specs
+        self.dst_layer = dst_layer
         self.mip = mip
         self.pad = pad
         self.bcube  = bcube
-        self.z_list = z_list
 
     def execute(self):
         padded_bcube = self.bcube.uncrop(self.pad, self.mip)
-        for k, z in enumerate(self.z_list[::-1]):
-            src_bcube = padded_bcube.reset_coords(zs=z, ze=z+1, in_place=False)
-            src_trans, src_data_dict = self.src_stack.read_data_dict(src_bcube,
-                                                           mip=self.mip, 
-                                                           stack_name='src')
-            agg_field = src_data_dict[f"src_agg_field"]
-            import pdb; pdb.set_trace()
-            img_name = self.src_stack.get_layers_of_type('img')[0].name
-            src_img = src_data_dict[f"src_{img_name}"]
-            mask_name = self.src_stack.get_layers_of_type('mask')[0].name
-            mask = src_data_dict[f"src_{mask_name}"]
-            warped_mask = residuals.res_warp_img(
-                mask.float(),
-                agg_field)
-            warped_mask = (warped_mask > 0.4).byte()
-            cropped_mask = helpers.crop(warped_mask, self.pad)
-            warped_src = residuals.res_warp_img(src_img.float(), agg_field)
-            cropped_src = helpers.crop(warped_src, self.pad)
-            # mask_layers = src_stack.get_layers_of_type(["mask"])
-            # mask = helpers.read_mask_list(mask_layers, src_bcube, self.mip)
+        for k, specs in enumerate(self.src_specs[::-1]):
+            z = specs['z']
+            mask_id = specs['mask_id']
+            bcube = padded_bcube.reset_coords(zs=z, ze=z+1, in_place=False)
+            imgs = {}
+            for name in ['img', 'mask', 'field']:
+                layer = self.src_layers[specs[name]]
+                if name == 'mask':
+                    layer.binarizer = helpers.Binarizer(['eq', mask_id])
+                imgs[name] = layer.read(bcube=bcube, mip=self.mip)
+            mask = residuals.res_warp_img(imgs['mask'].float(), imgs['field'])
+            mask = (mask > 0.4).byte()
+            cropped_mask = helpers.crop(mask, self.pad)
+            img = residuals.res_warp_img(imgs['img'].float(), imgs['field'])
+            cropped_img = helpers.crop(img, self.pad)
             if k == 0:
-                dst_image = cropped_src
-                dst_image[~cropped_mask] = 0
+                dst_img = cropped_img
+                dst_img[~cropped_mask] = 0
             else:
-                dst_image[cropped_mask] = cropped_src[cropped_mask]
+                dst_img[cropped_mask] = cropped_img[cropped_mask]
 
-        dst_layer = self.dst_stack.get_layers_of_type("img")[0]
-        dst_layer.write(dst_image, bcube=self.bcube, mip=self.mip)
+        self.dst_layer.write(dst_img, bcube=self.bcube, mip=self.mip)
 
 @click.command()
 @corgie_optgroup('Layer Parameters')
-@corgie_option('--src_layer_spec',  '-s', nargs=1,
-        type=str, required=True, multiple=True,
-        help='Source layer spec. Order img, mask, img, mask, etc. List must have length of multiple two.' + \
-                LAYER_HELP_STR)
-#
-@corgie_option('--dst_folder',  nargs=1,
-        type=str, required=False,
-        help= "Destination folder for the copied stack")
 @corgie_option('--spec_path',  nargs=1,
         type=str, required=True,
         help= "JSON spec relating src stacks, src z to dst z")
-
+@corgie_option('--dst_folder',  nargs=1,
+        type=str, required=False,
+        help= "Destination folder for the copied stack")
 @corgie_optgroup('Render Method Specification')
 @corgie_option('--chunk_xy',       '-c', nargs=1, type=int, default=1024)
 @corgie_option('--pad',                  nargs=1, type=int, default=512)
@@ -143,9 +136,8 @@ class MergeRenderTask(scheduling.Task):
 
 @click.pass_context
 def merge_render(ctx, 
-         src_layer_spec, 
-         dst_folder, 
          spec_path,
+         dst_folder, 
          chunk_xy, 
          pad,
          start_coord, 
@@ -161,55 +153,46 @@ def merge_render(ctx,
         suffix = f"_{suffix}"
 
     corgie_logger.debug("Setting up layers...")
-    assert(len(src_layer_spec) % 2 == 0)
-    src_stacks = {}
-    group = 3
-    for k in range(len(src_layer_spec) // group):
-        src_stack = create_stack_from_spec(src_layer_spec[group*k:group*(k+1)],
-                                            name='src', 
-                                            readonly=True)
-        name = src_stack.get_layers_of_type('img')[0].path
-        src_stacks[name] = src_stack
-
+    # create layers
+        # collect image paths
+        # collect mask paths
+    
     with open(spec_path, 'r') as f:
         spec = json.load(f)
 
-    # if force_chunk_xy:
-    #     force_chunk_xy = chunk_xy
-    # else:
-    #     force_chunk_xy = None
+    # collect {path: Layer}
+    src_layers = {}
+    for _, src_specs in spec.items():
+        for src_spec in src_specs:
+            for name in ['img','field','mask']:
+                src_path = src_spec[name]
+                if src_path not in src_layers:                        
+                    s = {'path': src_path, 'type': name}
+                    if name == 'mask':
+                        s['args'] = {'data_mip': 6}
+                    l = create_layer_from_dict(s, readonly=True)
+                    src_layers[src_path] = l
 
-    # if force_chunk_z:
-    #     force_chunk_z = chunk_z
-    # else:
-    #     force_chunk_z = None
-
-    dst_stack = stack.create_stack_from_reference(reference_stack=list(src_stacks.values())[0],
-                                                    folder=dst_folder, 
-                                                    name="dst", 
-                                                    types=["img"],
-                                                    readonly=False,
-                                                    suffix=suffix, 
-                                                    force_chunk_xy=None, 
-                                                    force_chunk_z =None,
-                                                    overwrite=True)
+    reference_layer = src_layers[list(src_layers.keys())[0]]
+    dst_layer = create_layer_from_dict({'path': dst_folder, 
+                                        'type': 'img'},
+                                       reference=reference_layer,
+                                       overwrite=True)
 
     bcube = get_bcube_from_coords(start_coord, end_coord, coord_mip)
 
     for z in range(*bcube.z_range()):
-        spec_z = str(z)
-        if spec_z in spec.keys():
-            src_dict = spec[str(z)]
+        tgt_z = str(z)
+        if tgt_z in spec.keys():
             job_bcube = bcube.reset_coords(zs=z, ze=z+1, in_place=False)
-            src_stack = src_stacks[src_dict['cv_path']]
-            z_list = src_dict['z_list']
-            render_job = MergeRenderJob(src_stack=src_stack,
-                               dst_stack=dst_stack,
-                               mip=mip,
-                               pad=pad,
-                               bcube=job_bcube,
-                               chunk_xy=chunk_xy,
-                               z_list=z_list)
-            # create scheduler and execute the job
-            scheduler.register_job(render_job, job_name="MergeRender {}".format(job_bcube))
+            render_job = MergeRenderJob(
+                            src_layers=src_layers,
+                            src_specs=spec[tgt_z],
+                            dst_layer=dst_layer,
+                            mip=mip,
+                            pad=pad,
+                            bcube=job_bcube,
+                            chunk_xy=chunk_xy)
+            scheduler.register_job(render_job, 
+                        job_name="MergeRender {}".format(job_bcube))
     scheduler.execute_until_completion()
